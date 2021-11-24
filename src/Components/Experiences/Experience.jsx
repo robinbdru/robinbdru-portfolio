1a8220ff5869d728711c5e52714af3d560a10e68
robinbdru <robinbeaudru@icloud.com>
2021-11-24 18:08:39 +0100

    Add Links to Exp.

--- src/Components/Experiences/Experience.jsx
+++ src/Components/Experiences/Experience.jsx
@@ -7,18 +7,25 @@ const experiences = [
     title: "Wild Code School",
     promotion: "2021 - Nantes",
     text: "La W.C.S. est une école de développement. Je participe au batch fall 2021, promotion développement web & mobile JS. L'école recense 20 campus partout en Europe.",
+    url: "https://www.wildcodeschool.com/",
   },
   {
     title: "1Kubator",
     promotion: "2020 - Nantes",
     text: "Je suis porteur de projet au sein de 1K, un incubateur-investisseur. Cette société accompagne et finance des projets startups en phase seed.",
+    url: "https://www.1kubator.com/",
   },
   {
     title: "Maia Mater",
     promotion: "2019 - Nantes",
     text: "Maia Mater est un programme destiné aux primo-entrepreneurs•es. Il s'agissait en 2019 d'un accompgnement 24/24 & 7/7 de 3 mois encadré par La Cantine du Numérique.",
+    url: "https://maiamater.lacantine.co/",
   },
 ];
+
+const handleClick = (url) => {
+  window.open(url, "_blank");
+};
 
 const Experience = () => {
   return (
@@ -35,9 +42,10 @@ const Experience = () => {
             personnes inspirantes, diverses et douées.
           </p>
         </div>
-        <div className='grid grid-cols-1 md:grid-cols-3 gap-4 text-left'>
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-4 text-left cursor-pointer'>
           {experiences.map((item, i) => (
             <div
+              onClick={() => handleClick(item.url)}
               key={i}
               className='group flex flex-col py-20 px-10 justify-end rounded-sm border transform border-gray-100 hover:bg-gray-200 hover:rounded-2xl  text-gray-500 hover:text-gray-900 h-auto hover:-translate-y-5 transition-all duration-300 ease-out'>
               <p className='mb-3 font-light text-xl text-left'>
